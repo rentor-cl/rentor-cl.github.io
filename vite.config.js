db85edbbdccbdf5e90a744469b97da11b5ca6b15
Diego-Hinrichs <diegofa.hinrichs@gmail.com>
2026-04-20 22:21:22 -0400

    Add README file and update Vite configuration base path

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// Cambia 'rentor-landing' por el nombre exacto de tu repositorio en GitHub
-const REPO_NAME = 'rentor-cl';
-
 export default defineConfig({
-  base: `/${REPO_NAME}/`,
+  base: '/',
   plugins: [react()],
 });
